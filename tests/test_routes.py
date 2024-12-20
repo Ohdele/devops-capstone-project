8d54a5653ab0a5e7b8db417ff970528df2f6f110
Ohdele <adesinaayodele48@gmail.com>
2024-12-20 02:11:04 -0500

    Fix linting issues and update pipeline configuration

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -22,6 +22,7 @@ BASE_URL = "/accounts"
 
 HTTPS_ENVIRON = {'wsgi.url_scheme': 'https'}
 
+
 class TestSecurityHeaders(TestCase):
     def setUp(self):
         """Set up test client"""
@@ -73,11 +74,11 @@ class TestAccountService(TestCase):
         db.session.remove()
 
     def test_cors_security(self):
-        """It should return a CORS header"""
-        response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        # Check for the CORS header
-        self.assertEqual(response.headers.get('Access-Control-Allow-Origin'), '*')    
+    """It should return a CORS header"""
+    response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
+    self.assertEqual(response.status_code, status.HTTP_200_OK)
+    # Check for the CORS header
+    self.assertEqual(response.headers.get('Access-Control-Allow-Origin'), '*')
 
     ######################################################################
     #  H E L P E R   M E T H O D S
